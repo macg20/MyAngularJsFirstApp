61b5c0d2e251417ac2101ff459de22ba22f1598b
kossss1 <kossss1@o2.pl>
2018-01-24 22:35:43 +0100

    next controllers filters and scope learning

--- app/controllers/firstController.js
+++ app/controllers/firstController.js
@@ -1,4 +1,8 @@
 var firstController = app.controller('firstController', function ($scope) {
     $scope.firstName = 'Em';
     $scope.lastName = 'Gie';
+
+    $scope.getFullName = function () {
+     $scope.firstAndLastName = $scope.firstName + $scope.lastName;
+    }
 });
---
+++ app/controllers/priceController.js
@@ -0,0 +1,3 @@
+app.controller("priceController", function ($scope) {
+    $scope.price = 4.58
+})
